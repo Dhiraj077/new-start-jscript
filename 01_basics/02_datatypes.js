2066d7b35be4bda0e466fc43673b424498e288fb
Dhiraj077 <93478391+Dhiraj077@users.noreply.github.com>
2025-12-17 18:11:39 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";  // treats all JS code as newer version
+
+// alert(3 + 3) // we're using nodejs, not browser
+
+console.log(3 +
+    3) // code readability should be high
+
+console.log('Hello');
+
+let name = 'Dhiraj'
+let age = 18
+let isLoggedIn = true
+let state
+
+
+/* DATATYPES */
+
+// Numbers => 2 to the power 53 range
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined  
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
