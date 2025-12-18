2fb90fae3dfdffd49a40c155eff9424c0cbc5a09
Dhiraj077 <93478391+Dhiraj077@users.noreply.github.com>
2025-12-18 18:34:20 +0000

    datatypesconversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "45ac" 
+
+// let score = 45 Result => number
+// let score = "45sf" Result => NaN
+// let score = null Result => 0
+// let score = undefined Result => NaN
+// let score = true/false Result => 1/0
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueINNumber = Number(score)
+console.log(typeof(valueINNumber));
+console.log(valueINNumber);
+
+let isLoggedIn = "Dhiraj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Dhiraj" => true
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
