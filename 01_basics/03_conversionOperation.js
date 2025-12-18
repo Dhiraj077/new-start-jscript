32c61aeb2308aecaa1a6ad3ba9811b9a9e20ec0b
Dhiraj077 <93478391+Dhiraj077@users.noreply.github.com>
2025-12-18 18:56:41 +0000

    change2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,6 +30,48 @@ console.log(typeof stringNumber);
 
 // ************* Operations ******************
 
+let value = 3
+
+let negvalue = -value
+
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hey"
+let str2 = " buddy"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 3);
+// console.log(1 + "3");
+// console.log("1" + 3 + 3);
+// console.log(1 + 3 + "4");
+
+// console.log((3 + 4) * 3 % 8);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
 
 
 
