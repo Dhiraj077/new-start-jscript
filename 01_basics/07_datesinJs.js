9799b65efc340a1392376b07b8579d6692ab4f88
Dhiraj077 <93478391+Dhiraj077@users.noreply.github.com>
2025-12-22 18:04:35 +0000

    dates

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,26 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+
+console.log(typeof myDate);
+
+//let myCreatedDate = new Date(2025, 1, 24)
+//let myCreatedDate = new Date(2025, 1, 24, 6, 3)
+let myCreatedDate = new Date('01-06-2025')
+
+console.log(myCreatedDate.toLocaleString());
+
+
+
+
+
+
+
