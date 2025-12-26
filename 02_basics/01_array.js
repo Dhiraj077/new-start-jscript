fd19f41ad3d3501d08128adc766083674148b084
Dhiraj077 <93478391+Dhiraj077@users.noreply.github.com>
2025-12-26 11:40:44 +0000

    array studied

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,5 +1,48 @@
 // Arrays
 
 const myArr = [1, 2, 3, 4, 5, true, 'string']
+const myHero = ['Ironman', 'Thor']
 
-console.log(myArr[6]);
+// console.log(myArr[2]);
+
+// Array Methods
+
+// myArr.push(23)
+// myArr.push(3) - add one more element at the end of the array
+// myArr.pop() - remove last element of an array
+
+// myArr.unshift(3) - add the element at the start
+// myArr.shift() - removes the 1st element
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C", myArr);
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
