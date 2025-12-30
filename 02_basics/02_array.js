b529dc95ca4fe1fa519dde2866256fdcc2b042a7
Dhiraj077 <93478391+Dhiraj077@users.noreply.github.com>
2025-12-30 10:04:10 +0000

    arrays study

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,5 +1,40 @@
 const marvel_heros = ["Thor", "Ironman", "Spiderman"]
 const dc_heros = ["Batman", "Suerman", "Flash"]
 
-marvel_heros.push(dc_heros)
-console.log(marvel_heros);
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+const all_heros = marvel_heros.concat(dc_heros)
+// console.log(all_heros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+
+// console.log(real_another_array);
+
+
+console.log(Array.isArray("Dhiraj"));
+console.log(Array.from("Dhiraj"));
+console.log(Array.from({name: "Dhiraj"})); // imp - gives empty array, to get value you've to give key
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+
+
+
+
+
+
+
+
