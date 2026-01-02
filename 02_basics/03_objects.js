1dd989ea14eb405508be54c0adbfe5556c6a55be
Dhiraj077 <93478391+Dhiraj077@users.noreply.github.com>
2026-01-02 12:11:44 +0000

    add objects studies

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,1 +1,40 @@
 // singleton
+
+
+// Object.create
+
+// object literals
+
+const mySym = Symbol('key1')
+
+const JsUser = {
+    name: 'Dhiraj',
+    'full name': 'Dhiraj Thakare',
+    [mySym]: 'mykey1',
+    age: '24',
+    location: 'Delhi',
+    email: 'sadasd@google.com',
+    isLoggedIn: false,
+    lastLoggedinDays: ['Monday', 'Saturday']
+}
+
+// console.log(JsUser.email);
+// console.log(JsUser['email']);
+// console.log(JsUser['full name']);
+// console.log(JsUser[mySym]);
+
+JsUser.email = 'fsdfdsfgr@gpt.com'
+Object.freeze(JsUser)
+JsUser.email = 'vrevwwe@micro.com'
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log('Hello JS users');
+}
+
+console.log(JsUser.greeting);
+
+
+
+
+
