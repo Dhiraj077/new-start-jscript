e9eaf52b31fb7dce4856aca116334e73551fe555
Dhiraj077 <93478391+Dhiraj077@users.noreply.github.com>
2026-03-12 10:28:23 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+// ****** While **************
+
+// let index = 0
+// while (index <= 10){
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ['Java', 'JavaScript', 'Python']
+
+let arr = 0
+while (arr < myArray.length){
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1 
+}
+
+
+// do while
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10)
