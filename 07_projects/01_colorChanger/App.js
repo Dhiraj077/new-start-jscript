2f6adf0857b5ebb1c4d13dfb0b7aafc96a2f9488
Dhiraj Thakre <93478391+Dhiraj077@users.noreply.github.com>
2026-06-08 10:50:50 +0000

    new projects

---
+++ 07_projects/01_colorChanger/App.js
@@ -0,0 +1,8 @@
+const buttons = querySelectorAll('.button');
+const body = querySelector('body');
+
+buttons.forEach(function (button){
+    console.log(button);
+    button.addEventListner()
+});
+
